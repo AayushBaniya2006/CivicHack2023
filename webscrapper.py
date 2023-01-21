c259234101c95a73d554843192de5348f41c3306
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2023-01-20 23:02:01 -0600

    webscrapper beginning
    added the beginning website get methods

--- webscrapper.py
+++ webscrapper.py
@@ -2,7 +2,7 @@ import pip._vendor.requests
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://do512.com/p/locally-owned-in-austin%27)
+r = requests.get('https://do512.com/p/locally-owned-in-austin%27')
 
 print(r)
 
