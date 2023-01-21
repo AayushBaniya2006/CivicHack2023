c40cc65d77f8059517a0871e372e8a15b707bef6
Johan_Park <johan.park97@gmail.com>
2023-01-21 16:01:45 -0600

    messed with sql_data

--- dataCollection/sql_data.py
+++ dataCollection/sql_data.py
@@ -22,14 +22,21 @@ def get_users_by_name(name):
     c.execute("SELECT * FROM employees WHERE username=:username", {'username': name})
     return c.fetchall()
     
-def update_users(user, name):
-    with conn:
-        c.execute("""UPDATE users SET username = :username
+def update_username(user, name):
+        with conn:
+            c.execute("""UPDATE users SET username = :username
                   WHERE password = :password AND email = :email""",
                   {'password': user.password, 'email': user.email, 'username': name})
+        
+def update_userpassword(user, password):
+    with conn:
+        c.execute("""UPDATE users SET password = :password
+                  WHERE username = :username AND email = :email""",
+                  {'username': user.username, 'email': user.email, 'username': password})
 
 def remove_user(user):
     with conn:
         c.execute("DELETE from users WHERE username = :username AND password = :password",
                   {'username':user.first, 'password': user.password})
 
+#incomplete password and username checker
