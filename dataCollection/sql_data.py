979d6da1fd4fee2f7d92aa2d89f4eef2dcc2bfdc
Johan_Park <johan.park97@gmail.com>
2023-01-21 17:03:42 -0600

    Edited sql_data

--- dataCollection/sql_data.py
+++ dataCollection/sql_data.py
@@ -14,29 +14,37 @@ c = conn.cursor()
 #            email TEXT
 #            )""")
 
+#Inserts a new user into the SQl database
 def insert_user(user):
     with conn:
         c.execute("INSERT INTO users VALUES (:username, :password : email)", {'username': user.username, 'password': user.password, 'email': user.email})
 
+#Looks through the database to find users whose usernames = name
 def get_users_by_name(name):
     c.execute("SELECT * FROM employees WHERE username=:username", {'username': name})
     return c.fetchall()
     
+#Changes username
 def update_username(user, name):
         with conn:
             c.execute("""UPDATE users SET username = :username
                   WHERE password = :password AND email = :email""",
                   {'password': user.password, 'email': user.email, 'username': name})
-        
+#Similar to username -> Changes password
 def update_userpassword(user, password):
     with conn:
         c.execute("""UPDATE users SET password = :password
                   WHERE username = :username AND email = :email""",
                   {'username': user.username, 'email': user.email, 'username': password})
-
+#Deletes users whose usernames and passwords are equal to the user compared to.
 def remove_user(user):
     with conn:
         c.execute("DELETE from users WHERE username = :username AND password = :password",
                   {'username':user.first, 'password': user.password})
 
 #incomplete password and username checker
+def check_Credentials(name, password):
+    user = get_users_by_name(name)
+    if user.getName() == name or user.getPassword() == password:
+        return True
+    return False
