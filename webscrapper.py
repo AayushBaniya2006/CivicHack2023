fe0d90d9dba6b497ae270a12d7dc21f7dbc3fe5e
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2023-01-20 23:23:32 -0600

    Update webscrapper.py

--- webscrapper.py
+++ webscrapper.py
@@ -1,3 +1,20 @@
+import re
+import json
+from urllib2 import urlopen
+
+url = 'http://ipinfo.io/json'
+response = urlopen(url)
+data = json.load(response)
+
+IP=data['ip']
+org=data['org']
+city = data['city']
+country=data['country']
+region=data['region']
+
+print('Your IP detail\n ')
+print('IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP))
+
 import pip._vendor.requests 
 import requests
 from bs4 import BeautifulSoup
@@ -7,5 +24,6 @@ r = requests.get('https://do512.com/p/locally-owned-in-austin%27')
 
 
 soup = BeautifulSoup(r.content, "html.parser")
-print(soup.prettify())
-print("x")
+
+for link in soup.find_all('p'):
+    print(link)
