01a2315c07821dc3f55d792eb05685562e7d09e3
Johan_Park <johan.park97@gmail.com>
2023-01-20 23:55:21 -0600

    Changes to main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pip._vendor.requests 
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://do512.com/p/locally-owned-in-austin%27')
+
+print(r)
+
+soup = BeautifulSoup(r.content, "html.parser")
+print(soup.prettify())
