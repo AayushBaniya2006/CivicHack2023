9d4b5a929c15183cccbb95f992030b3d9b8b1f95
Johan_Park <johan.park97@gmail.com>
2023-01-22 02:10:34 -0600

    Finished Flask tutorial

--- Flask tutorials n stuff/flaskTutorial.py
+++ Flask tutorials n stuff/flaskTutorial.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 #Routing -> Ties a url from a HTML website to  Python function. I think .route is the routing
 
 #@ signifies a decorator - a decorator is a way to wrap a function and modify its behavior
-@app.route('/') #Connects the main page of the HTML to this function? Not sure on this part :/
-def index(): #Defines the main page
-    return 'This is the homepage' # This is what is returned when we head to the main(home page)
+#@app.route('/') #Routes to main page to website
+#def index(): #Defines the main page
+#    return 'This is the homepage' # This is what is returned when we head to the main(home page)
 
 @app.route('/tuna')#Edit this later -> This basically sets a route to a another page from main. change the words behind the / to change the page you want to go to
 def tuna():#Function doesn't have to be same name as HTML link
@@ -33,8 +33,20 @@ def bacon():
 @app.route('/profile/<name>', methods=['GET'])
 def otherProfile(name):
     return render_template("profile.html", substitute = name) #The render_template basically will have the webpage upload whatever html file have it render. In this case, it's profile.html
-#The name=name is there to define what information needs to be displayed in the html layout. So the name is displayed.
+#The substitute=name is there to define what information needs to be displayed in the html layout. So the name is displayed.
 
-if __name__== "__main__": #This check make sure that the webserver when this file is called directly
+#Routes multipe HTML pages to one function. So both / & /<user> are both links to the main page(Like how being signed in and being signed out have two different web pages)
+@app.route('/')
+@app.route('/<user>')
+def index(user=None): #Defaults tbe user into being nothing.
+    return render_template('user.html', user=user)
+
+#How to load lists into a html webpage
+@app.route('/shopping')
+def shopping():
+    food = ['Cheese', 'Tuna', 'Beef'] 
+    return render_template('shoppinglist.html', food=food)
+
+if __name__== "__main__": #This check make sure that the webserver runs only when this file is called directly
     app.run(debug=True) #This starts the app
     
