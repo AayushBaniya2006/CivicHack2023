4a27895e13e1dfe528fc1288bde8f7ab01383872
Qayshawwar <qayshawwar@gmail.com>
2023-01-22 04:06:45 -0600

    it works

--- webscrapperTest.py
+++ webscrapperTest.py
@@ -1,9 +1,15 @@
 import requests
 
-url = "https://api.fda.gov/drug/drugsfda.json?limit=5"
+url = "https://api.fda.gov/drug/drugsfda.json?limit=68"
 response = requests.get(url)
 data = response.json()
 
+drugNeeded = input("Enter drug: ")
 for drug in data["results"]:
     for product in drug["products"]:
-        print(product["brand_name"])
+        if drugNeeded.upper() == product["brand_name"]:
+            print("Method of Consumption: " + product["dosage_form"])
+            print("Indredients:")
+            for ingredient in product["active_ingredients"]:
+                print(ingredient["strength"]+" " + ingredient["name"])
+            break
