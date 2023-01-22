f2713b152f99ad1242a59632a400bde710bd77ef
AayushBaniya2006 <thisisaayushbaniya@gmail.com>
2023-01-22 06:41:49 -0600

    Update TestMain.html

--- webscrapperTest.py
+++ webscrapperTest.py
@@ -5,26 +5,6 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('TestMain.html'))
 
 
-<<<<<<< Updated upstream
-url = "https://api.fda.gov/drug/drugsfda.json?limit=68"
-response = requests.get(url)
-data = response.json()
-
-template = env.get_template('TestMain.html')
-data = "This is data from python"
-output = template.render(data=data)
-
-drugNeeded = input("Enter drug: ")
-for drug in data["results"]:
-    for product in drug["products"]:
-        if drugNeeded.upper() == product["brand_name"]:
-            print("Method of Consumption: " + product["dosage_form"])
-            print("Indredients:")
-            for ingredient in product["active_ingredients"]:
-                print(ingredient["strength"]+" " + ingredient["name"])
-            break
-
-=======
 
 app = Flask(__name__)
 
@@ -48,4 +28,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> Stashed changes
