fd5aa37c604432792b17b8837bddaeef81c37570
AayushBaniya2006 <thisisaayushbaniya@gmail.com>
2023-01-21 23:16:07 -0600

    idek bruh

--- OpenFDA.py
+++ OpenFDA.py
@@ -1,27 +1,20 @@
-import requests
-import json
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
-# Get the drug name and location from the user
 drug_name = input("Enter the name of the drug: ")
-location = input("Enter your location (zip code or city, state): ")
 
-# Set the API endpoint
-api_endpoint = "https://www.goodrx.com/api/compare-prices/"
+html = "https://www.goodrx.com/" + drug_name
 
-# Set the API parameters
-params = {'name': drug_name, 'location': location}
 
-# Make the API call
-response = requests.get(api_endpoint, params=params)
+driver = webdriver.Chrome()
+driver.get(html)
+time.sleep(3)
 
-# Check if the API returned a successful response
-if response.status_code == 200:
-    # Parse the JSON data
-    data = json.loads(response.text)
-    # Extract the cheapest price from the JSON data
-    prices = data['prices']
-    cheapest_price = min(prices, key=lambda x: x['price'])
-    # Print the cheapest price
-    print("The cheapest price for " + drug_name + " in " + location + " is $" + str(cheapest_price['price']))
-else:
-    print("Error: Could not retrieve data from the API. ")
+soup = BeautifulSoup(driver.page_source,'lxml')
+
+for a in soup.find_all('a',href=True):
+
+    print ("Found the URL:",a['href'])
+
+driver.close()
