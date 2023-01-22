dceced7c206e231412740a5b9245e681f5715c5b
Qayshawwar <qayshawwar@gmail.com>
2023-01-22 01:59:05 -0600

    searchbar stuff

--- OpenFDA.py
+++ OpenFDA.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
+<<<<<<< Updated upstream
 drug_name = input("Enter the name of the drug: ")
 
 html = "https://www.goodrx.com/" + drug_name
@@ -17,4 +18,27 @@ for a in soup.find_all('a',href=True):
 
     print ("Found the URL:",a['href'])
 
-driver.close()
+=======
+# Get the drug name from the user
+drug_name = input("Enter the name of the drug: ")
+
+# Set the API endpoint
+api_endpoint = "https://data.medicaid.gov/resource/nqzp-d4kc.json"
+
+# Set the API parameters
+params = {'name': drug_name}
+
+# Make the API call
+response = requests.get(api_endpoint, params=params)
+
+# Check if the API returned a successful response
+if response.status_code == 200:
+    # Parse the JSON data
+    data = json.loads(response.text)
+    # Extract the NADAC price from the JSON data
+    NADAC_price = data[0]['nadac_per_unit']
+    # Print the NADAC price
+    print("The NADAC price for " + drug_name + " is $" + NADAC_price)
+else:
+    print("Error: Could not retrieve data from the API.")
+>>>>>>> Stashed changes
--- TestApi.py
+++ TestApi.py
@@ -1,4 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://api.fda.gov/drug/event.json?limit=1",params="advil")
-print(response.text)
+# Get the drug name from the user
+drug_name = input("Enter the name of the drug: ")
+
+# Set the GoodRx URL
+url = "https://www.goodrx.com/" + drug_name
+
+# Make the GET request to the website
+response = requests.get(url)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find the price element
+price_element = soup.find("span", {"class": "drug-price_price"})
+
+# Extract the price from the element
+price = price_element.text
+
+# Print the price
+print("The price for " + drug_name + " on GoodRx is: " + price)
