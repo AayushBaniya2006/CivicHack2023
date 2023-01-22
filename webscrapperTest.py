8baa9e9e05ba9e104c31fcbe38d6d774145bdfe7
Qayshawwar <qayshawwar@gmail.com>
2023-01-22 06:55:04 -0600

    it worked again

--- webscrapperTest.py
+++ webscrapperTest.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request
 import requests
 
-url = "https://api.fda.gov/drug/drugsfda.json?limit=68"
-response = requests.get(url)
-data = response.json()
+app = Flask(__name__)
 
-template = env.get_template('TestMain.html')
-data = "This is data from python"
-output = template.render(data=data)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        drugNeeded = request.form['drug']
+        url = f"https://api.fda.gov/drug/drugsfda.json?limit=68&search={drugNeeded}"
+        response = requests.get(url)
+        data = response.json()
 
-drugNeeded = input("Enter drug: ")
-for drug in data["results"]:
-    for product in drug["products"]:
-        if drugNeeded.upper() == product["brand_name"]:
-            print("Method of Consumption: " + product["dosage_form"])
-            print("Indredients:")
-            for ingredient in product["active_ingredients"]:
-                print(ingredient["strength"]+" " + ingredient["name"])
-            break
+        for drug in data["results"]:
+            for product in drug["products"]:
+                if drugNeeded.upper() == product["brand_name"]:
+                    method_of_consumption = product["dosage_form"]
+                    ingredients = []
+                    for ingredient in product["active_ingredients"]:
+                        ingredients.append(f"{ingredient['strength']} {ingredient['name']}")
+                    return render_template('result.html', method_of_consumption=method_of_consumption, ingredients=ingredients)
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
